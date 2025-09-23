75b43f21690033227f962c0003540ae3e2fa0055
David <davik50@gmail.com>
2025-09-23 20:35:04 +0200

    CBG - edits 3

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     kotlin("jvm") version "2.0.0"
     application
 }
-
+layout.buildDirectory.set(file("out"))
 repositories {
     mavenCentral()
 }
@@ -38,4 +38,4 @@ tasks.test {
 }
 
 
-kotlin { jvmToolchain(17) }
+kotlin { jvmToolchain(21) }
